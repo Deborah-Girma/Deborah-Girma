908f42e8d318210e5addd1595bc0bbc0a84dd44c
Deborah Girma <diboragk12@gmail.com>
2024-06-21 15:29:57 +0300

    Update contact_script.js

--- contact_script.js
+++ contact_script.js
@@ -74,8 +74,8 @@ form.addEventListener('submit', async (event) => {
     }
 });
 
-// Email validation function (optional, can be improved)
+// Email validation function (improved)
 function validateEmail(email) {
-    const re = /^(([^<>()[\\]\\\\.,;:\s@"]+(\.[^<>()[\\]\\\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
 }
